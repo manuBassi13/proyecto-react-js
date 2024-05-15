84056f76deaff77d61d2fc3bfed90c56cd1708d6
Manuel Bassi <manuelbassi_8@hotmail.com>
2024-05-14 23:12:22 -0300

    Mejora en nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,29 +5,31 @@ import { CartWidget } from "../CartWidget/CartWidget.jsx"
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
 export const Navigation = () => {
     return (
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container className="bg-dark">
-                <Navbar.Brand href="#home">
-                    <img src={logoBS} alt="Logo Black Sheep" className={styles.navbarLogo} />
+        <Navbar expand="lg" className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={logoBS} width="100px" alt="Logo Black Sheep"  />
                 </Navbar.Brand>
-            
-                <Nav className="me-auto dark">
-                        <Link to="/">Inicio </Link>
-                        <Link to="/products">Products</Link>
-                        <Link to="/category/canastos">Canastos</Link>
-                        <Link to="/category/personal">Uso Personal</Link>
-                        <Link to="/category/ceramica">Ceramica</Link>
-                        <Link to="/category/textil">Textil</Link>
-                        <Link to="/cart">Cart</Link>
-                        <Link to="/about">About us</Link>
-                        <Link to="/dashboard">Dash</Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className={styles.navLinks}>
+                            <Link to="/">Inicio</Link>
+                            <Link to="/products">Productos</Link>
+                            <Link to="/category/canastos">Canastos</Link>
+                            <Link to="/category/personal">Uso Personal</Link>
+                            <Link to="/category/ceramica">Ceramica</Link>
+                            <Link to="/category/textil">Textil</Link>
+                            <Link to="/cart">Cart</Link>
+                            <Link to="/about">About us</Link>
+                            <Link to="/dashboard">Dash</Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
             <CartWidget/>
         </Navbar>
